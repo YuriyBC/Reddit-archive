32ef3c82c51e0d9008fa02e7bdeb4ff88288814c
yuriybc <yurikwbc@gmail.com>
2019-01-21 15:13:48 +0300

    fix database config

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const apiService = require('./services/apiService');
 const databaseService = require('./services/databaseService');
 const redditFetcherService = require('./services/redditService');
+const mysqlApostrophe = require("mysql-apostrophe");
 const websocketService = require('./services/websocketService');
 const port = process.env.PORT || 8080;
 const app = express();
@@ -14,6 +15,7 @@ const {SUBREDDITS_TABLE_TITLE} = constants;
 app.use(express.static(`${__dirname}/../public/public`));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(mysqlApostrophe)
 
 const server = http.createServer(app);
 
