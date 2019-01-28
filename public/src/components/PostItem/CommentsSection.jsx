43d1ba351ed28a538520ec903b5a37b39459d126
yuriybc <yurikwbc@gmail.com>
2019-01-28 18:01:55 +0300

    added comments lazy loading

--- public/src/components/PostItem/CommentsSection.jsx
+++ public/src/components/PostItem/CommentsSection.jsx
@@ -6,11 +6,18 @@ const { sortComments } = methods;
 export default class CommentsSection extends React.Component {
     constructor (props) {
         super(props);
+        this.commentToShowStep = 20;
         this.getComments = this.getComments.bind(this);
+        this.showMoreComments = this.showMoreComments.bind(this);
+        this.state = {
+            commentsToShow: this.commentToShowStep
+        }
     }
 
     getComments () {
         let comments = sortComments([...this.props.comments]);
+        comments = comments.slice(0, this.state.commentsToShow);
+
         if (comments) {
             return comments.map((comment, index) => {
                 if (comment.body !== 'null') {
@@ -20,9 +27,26 @@ export default class CommentsSection extends React.Component {
         }
     }
 
+    getLoadMoreButtonStyle () {
+        return {
+            'display': this.state.commentsToShow < this.props.comments.length ? 'block' : 'none'
+        }
+    }
+
+    showMoreComments () {
+        this.setState({
+            commentsToShow: this.state.commentsToShow + this.commentToShowStep
+        })
+    }
+
     render () {
         return <div className="comments-section box">
             {this.getComments()}
+            <span onClick={this.showMoreComments}
+                  style={this.getLoadMoreButtonStyle.call(this)}
+                  className="comments-section__button">
+                Load more comments
+            </span>
         </div>
     }
 }
